4ae0c6438e6997d4ca3ed00c51ca11ddc6b835aa
saketksingh <saketsingh2018@gmail.com>
2018-02-03 19:09:39 -0600

    Initial Commit

---
+++ cinemaboss/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cinemaboss
+//
+//  Created by somi on 2/3/18.
+//  Copyright © 2018 Somi Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
