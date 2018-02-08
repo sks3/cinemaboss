f35176b18a0b0c4bde3cd83603511790138a5889
saketksingh <saketsingh2018@gmail.com>
2018-02-08 16:41:57 -0600

    Implement TableView and API Request

--- cinemaboss/NowPlayingViewController.swift
+++ cinemaboss/NowPlayingViewController.swift
@@ -1,45 +1,87 @@
-//
-//  NowPlayingViewController.swift
-//  cinemaboss
-//
-//  Created by somi on 2/4/18.
-//  Copyright © 2018 Somi Singh. All rights reserved.
-//
+/// Copyright (c) 2018 Somi Singh
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
 
 import UIKit
 
-class NowPlayingViewController: UIViewController {
+class NowPlayingViewController: UIViewController, UITableViewDataSource {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-      
-      let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=4e92dd6c397483b130eb698d2e0bb14e")!
-      
-      let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-      
-      let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-      
-      let task = session.dataTask(with: request) { (data, response, error) in
-        // This will run when the network request returns
-        if let error = error {
-          print(error.localizedDescription)
-        }
-        else if let data = data {
-          let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-          print(dataDictionary)
-        }
-      }
-      task.resume()
-
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+  @IBOutlet weak var tableView: UITableView!
+  
+  var movies: [[String: Any]] = []
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-
-
-
+    // define datasource for tableview
+    tableView.dataSource = self
+    tableView.rowHeight = 190
+  
+    let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=4e92dd6c397483b130eb698d2e0bb14e")!
+    
+    let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+    
+    let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+    
+    let task = session.dataTask(with: request) { (data, response, error) in
+      // This will run when the network request returns
+      if let error = error {
+        print(error.localizedDescription)
+      }
+      else if let data = data {
+        let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+        let movies = dataDictionary["results"] as! [[String: Any]]
+        self.movies = movies
+        self.tableView.reloadData()
+      }
+    }
+    task.resume()
+    
+    }
+  
+  // default tableView functions
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return movies.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+    let movie = movies[indexPath.row]
+    let title = movie["title"] as! String
+    let overview = movie["overview"] as! String    
+    cell.titleLabel.text = title
+    cell.overviewLabel.text = overview
+    return cell
+  }
+  
+  override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+  }
+    
 }
 
