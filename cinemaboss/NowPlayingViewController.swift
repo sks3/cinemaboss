ac07c54228175d3034f0de376abb2b14c8cd067c
sks3 <saketsingh2018@gmail.com>
2018-02-11 22:11:55 -0600

    Implement Pull to Refresh

--- cinemaboss/NowPlayingViewController.swift
+++ cinemaboss/NowPlayingViewController.swift
@@ -34,20 +34,29 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
   @IBOutlet weak var tableView: UITableView!
   
   var movies: [[String: Any]] = []
-  
+  var refreshControl: UIRefreshControl!
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    refreshControl = UIRefreshControl()
+    refreshControl.addTarget(self, action: #selector
+      (NowPlayingViewController.didPullToRefresh(_:)), for: .valueChanged)
     // define datasource for tableview
+    tableView.insertSubview(refreshControl, at: 0)
     tableView.dataSource = self
     tableView.rowHeight = 190
+    fetchNowPlayingMovies()
+    
+  }
   
+  @objc func didPullToRefresh(_ refreshControl: UIRefreshControl) {
+    fetchNowPlayingMovies()
+  }
+  
+  func fetchNowPlayingMovies() {
     let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=4e92dd6c397483b130eb698d2e0bb14e")!
-    
     let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
-    
     let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
-    
     let task = session.dataTask(with: request) { (data, response, error) in
       // This will run when the network request returns
       if let error = error {
@@ -58,11 +67,11 @@ class NowPlayingViewController: UIViewController, UITableViewDataSource {
         let movies = dataDictionary["results"] as! [[String: Any]]
         self.movies = movies
         self.tableView.reloadData()
+        self.refreshControl.endRefreshing()
       }
     }
     task.resume()
-    
-    }
+  }
   
   // default tableView functions
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
